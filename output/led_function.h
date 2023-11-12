3b3fdefb39d424197e15ae70493bd34456ac0339
javaldrnld <rjavalde9@gmail.com>
2023-11-12 19:02:42 +0800

    - Nag sync ba?

--- output/led_function.h
+++ output/led_function.h
@@ -4,6 +4,4 @@
 void control_LED(int setup_LED, int setup_LDR, int LDR_BRIGHT_THRESHOLD);
 int read_LDR(int setup_LDR);
 
-
-
 #endif
--- output/soil_functions.h
+++ output/soil_functions.h
@@ -1,8 +1,6 @@
 #ifndef SOIL_FUNCTIONS_H
 #define SOIL_FUNCTIONS_H
 
-// Code Here
-
 void setup_moisture_control(int valve_pin, int soil_sensor1, int soil_sensor2); // Valve 
 void read_moisture_control_valve(int valve_pin, int soil_sensor1, int soil_sensor2);
 
